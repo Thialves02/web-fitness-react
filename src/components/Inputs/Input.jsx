3dc397f4f23baac276b5948adfa7c121b63f0008
Thiago <thiago.ralves02@gmail.com>
2022-09-17 20:59:22 -0300

    feat: recebimento de rest

--- src/components/Inputs/Input.jsx
+++ src/components/Inputs/Input.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import { InputContainer } from './style'
 import { ErrorMessage, Field } from 'formik'
 
-export default function Input({ label, name, placeholder, maxLength }) {
+export default function Input({ label, name, placeholder, maxLength, ...rest }) {
     return (
         <InputContainer>
             <label>{label}</label>
-            <Field type="text" name={name} placeholder={placeholder} maxLength={maxLength} />
+            <Field type="text" name={name} placeholder={placeholder} maxLength={maxLength} {...rest} />
             <ErrorMessage
                 component="span"
                 name={name}
