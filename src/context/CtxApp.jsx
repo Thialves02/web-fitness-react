85cee77ebec211d5fb87935651c47b1ea9eb2c0b
Thiago <thiago.ralves02@gmail.com>
2022-09-17 21:00:51 -0300

    feat: armazenamento de id

--- src/context/CtxApp.jsx
+++ src/context/CtxApp.jsx
@@ -5,9 +5,23 @@ const Context = createContext();
 
 export default function CtxApp({ children }) {
     const [jwt, setJwt, jwtRemove] = useLocalStorage('JWT', null)
+    const [id, setId, idRemove] = useLocalStorage('id', null)
+    const [peso, setPeso] = useState('')
+    const [idade, setIdade] = useState('')
+    const [altura, setAltura] = useState('')
+    const [nivelAtividade, setNivelAtividade] = useState('')
+    const [objetivo, setObjetivo] = useState('')
+    const [genero, setGenero] = useState('')
+    const [nome, setNome] = useState('')
+
     return (
         <Context.Provider
-            value={{ jwt, setJwt, jwtRemove }}
+            value={{
+                jwt, setJwt, jwtRemove, id, setId, idRemove,
+                peso, setPeso, idade, setIdade, altura, setAltura,
+                nivelAtividade, setNivelAtividade, objetivo, setObjetivo,
+                genero, setGenero, nome, setNome
+            }}
         >
             {children}
         </Context.Provider>
